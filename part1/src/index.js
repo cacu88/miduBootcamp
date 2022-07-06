e8df6b6cdc459ccc1c3cb80e9677f650f0811506
Facundo Sevrini <facuse88@gmail.com>
2022-07-06 04:54:15 -0300

    render state

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,21 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react' //estado
 import ReactDOM from 'react-dom'
 
-const App = (props) => {
-    const {counter} = props
-    return (
+const App = () => {
+  const [ counter, setCounter ] = useState(0)
+  
+  setTimeout(    
+    () => setCounter(counter + 1),    
+    1000
+  )
+
+  console.log('rendering...', counter)
+  
+  return (
       <div>{counter}</div>
     )
-  }
+}
   
+ReactDOM.render(
+    <App />, 
+    document.getElementById('root')
+)
+
+  /*
   let counter = 1
-  
-const refresh = () => {
+  const refresh = () => {
     ReactDOM.render(<App counter={counter} />, 
     document.getElementById('root'))
-}
-
-setInterval(() => {
+  }
+  
+  setInterval(() => {
     refresh()
     counter += 1
   }, 1000)
+  */
