f8706bbe14578d4018c52ba72c04df70c1d4e238
Facundo Sevrini <facuse88@gmail.com>
2022-07-11 15:09:37 -0300

    Estado comp controlador eventos

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,18 +1,47 @@
 import React, { useState } from 'react' //estado
 import ReactDOM from 'react-dom'
 
+const Display = ({counter}) => <div>{counter}</div>
+
+const Button = ({handleClick,text}) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
 const App = () => {
   const [ counter, setCounter ] = useState(0)
-  
+
+  /*  
   setTimeout(    
     () => setCounter(counter + 1),    
     1000
   )
 
   console.log('rendering...', counter)
-  
+  */ //componente estado
+
+  //const handleClick = () => {    
+  //  console.log('clicked')  
+  //}
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const setToZero = () => setCounter(0)
+  const decreaseByOne = () => setCounter(counter - 1)
+
   return (
-      <div>{counter}</div>
+    <div>
+      <Display counter={counter}/>
+      <Button handleClick ={increaseByOne} text="Plus"/>
+      <Button handleClick ={decreaseByOne} text="Minus"/>
+      <Button handleClick ={setToZero} text="Zero"/>
+      {/*<button onClick={increaseByOne}>
+        plus
+      </button>
+      <button onClick={setToZero}>
+        zero      
+  </button>*/}
+    </div>
     )
 }
   
